600f3f27ad667e9b78f394b1bf08624175827b67
QBit <ivs.tech.24@gmail.com>
2024-04-10 20:26:08 +0530

    improvement

--- BrickOrz.py
+++ BrickOrz.py
@@ -32,7 +32,7 @@ async def on_message(message):
     if message.author.bot:
         return
     
-    print(f'Message from {message.author}: {message.content}')
+    # print(f'Message from {message.author}: {message.content}')
     
     # Add your logic for message reactions and other responses
 
