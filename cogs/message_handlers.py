b30c42a8e160d0f6ab4f71f932d2072139d95a47
QBit <ivs.tech.24@gmail.com>
2024-04-14 03:28:25 +0530

    bug fix

--- cogs/message_handlers.py
+++ cogs/message_handlers.py
@@ -10,13 +10,13 @@ class MessageHandlers(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
+        if message.channel.id == NOTIFICATION_CHANNEL_ID:
+            await message.add_reaction('✅')
+
         if message.author.bot:
             return
 
         await self.bot.process_commands(message)
-
-        if message.channel.id == NOTIFICATION_CHANNEL_ID:
-            await message.add_reaction('✅')
 
         if message.channel.id == WELCOME_CHANNEL_ID:
             emojis = ['👋', '<:blobbeat:1206994557137326110>', '\<:catKing:1206994563567452220>', '<:AC:1206994544306954313>', '<:ghosthug:1206994583578349598>', '\<:gigachad:1206994586636001281>', '\<:hype:1206994599885672519>', '\<:pkinglove:1206994630600818748>', '\<:prayge:1206994633305882686>', '<:proud:1206994636560670720>', '\<:redHandWin:1206994645016518656>', '\<:sir:1206994677644001361>', '\<:stickManLove:1206994683516157953>', '\<:yay:1206994703115886593>', '\<:yayy:1206994707020914719>', '\<:yesSir:1206994714570788874>']
@@ -32,11 +32,10 @@ class MessageHandlers(commands.Cog):
 
     async def handle_discord_invite(self, message):
         try:
-            await message.author.send(f"Hello {message.author.name}, 🚫 posting other Discord server invites is not allowed in '{message.guild.name}', to prevent spam! If the invite link is very important then you can send it to the respective person in DM 😳")
             log_channel = self.bot.get_channel(LOG_CHANNEL_ID)
-            if log_channel:
-                await log_channel.send(f"Invite link posted by {message.author} ({message.author.id}) in {message.channel} 🫣")
+            await log_channel.send(f"Invite link posted by {message.author} ({message.author.id}) in {message.channel} 🫣")
             await message.delete()
+            await message.author.send(f"Hello {message.author.name}, 🚫 posting other Discord server invites is not allowed in '{message.guild.name}', to prevent spam! If the invite link is very important then you can send it to the respective person in DM 😳")
         except discord.HTTPException as e:
             print(f"Error: {e}")
 
--- cogs/reaction_handlers.py
+++ cogs/reaction_handlers.py
@@ -13,7 +13,7 @@ class ReactionHandlers(commands.Cog):
         if user.bot or reaction.message.id in self.sent_messages:
             return
 
-        if reaction.count >= 2:
+        if reaction.count >= 3:
             reaction_board_channel = discord.utils.get(reaction.message.guild.channels, name=EMOJI_BOARD_CHANNEL_NAME)
             if not reaction_board_channel:
                 print(f"Channel not found: {EMOJI_BOARD_CHANNEL_NAME}")
